d1f623ad9fd496724e9b67b111752a2e036cef6a
Lukasz Bezlada <bezi@o2.pl>
2019-04-14 17:58:51 +0200

    build maven project for rest api

---
+++ target/restapi/java/com/infoshareacademy/App.java
@@ -0,0 +1,11 @@
+package com.infoshareacademy;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/v1/")
+public class App extends Application {
+    public static void main(String[] args) {
+
+    }
+}
